53795a0d3a6f8430b381b8f49f6ad2006c7c2ebc
MiguelJDelgado <migueljd193@gmail.com>
2025-07-31 17:32:14 -0300

    Áprimorando erros de validação em POST livros e autores

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,8 +1,16 @@
 import mongoose from "mongoose";
 
 function manipuladorDeErros (erro, req, res, next){
+    console.log(erro);
     if (erro instanceof mongoose.Error.CastError) {
-        res.status(400).send({message: "Um ou mais dados fornecidos estão incorretos."});
+      res.status(400).send({message: "Um ou mais dados fornecidos estão incorretos."});
+    } else if (erro instanceof mongoose.Error.ValidationError){
+
+        const mensagensErro = Object.values(erro.errors)
+            .map(erro => erro.message)
+            .join(" - ")
+
+        res.status(400).send({message: `Os seguintes erros foram encontrados: ${mensagensErro}`});
     } else {
         res.status(500).send({message: "Erro interno do servidor."});
     }
