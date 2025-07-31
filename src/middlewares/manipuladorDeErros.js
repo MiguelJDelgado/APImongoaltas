4778cb8249743e0b15ab98efa99d697a6075be01
MiguelJDelgado <migueljd193@gmail.com>
2025-07-31 18:10:13 -0300

    manipuladorDeErros.js refatorado

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,18 +1,16 @@
 import mongoose from "mongoose";
+import ErroBase from "../Erros/ErroBase.js";
+import RequisicaoIncorreta from "../Erros/RequisicaoIncorreta.js";
+import ErroValidacao from "../Erros/ErroValidacao.js";
 
 function manipuladorDeErros (erro, req, res, next){
     console.log(erro);
     if (erro instanceof mongoose.Error.CastError) {
-      res.status(400).send({message: "Um ou mais dados fornecidos estão incorretos."});
+      new RequisicaoIncorreta().enviarResposta(res);
     } else if (erro instanceof mongoose.Error.ValidationError){
-
-        const mensagensErro = Object.values(erro.errors)
-            .map(erro => erro.message)
-            .join(" - ")
-
-        res.status(400).send({message: `Os seguintes erros foram encontrados: ${mensagensErro}`});
+        new ErroValidacao(erro).enviarResposta(res);
     } else {
-        res.status(500).send({message: "Erro interno do servidor."});
+        new ErroBase().enviarResposta(res);
     }
 }
 
