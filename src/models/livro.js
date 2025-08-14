3d7edb0271c067e18da3b8145c1dffe9f89a5d01
MiguelJDelgado <migueljd193@gmail.com>
2025-08-14 16:58:40 -0300

    Adicionando validações do mongoose

--- src/models/livro.js
+++ src/models/livro.js
@@ -5,18 +5,36 @@ const livroSchema = new mongoose.Schema(
     id: {type: String},
     titulo: {
       type: String, 
-      required: [true, "O título do livro é obrigatório"]},
+      required: [true, "O título do livro é obrigatório"]
+    },
     preco: {
       type: Number,
-      required: [true, "O preço do livro é obrigatório"]},
+      required: [true, "O preço do livro é obrigatório"]
+    },
     autor: {
       type: mongoose.Schema.Types.ObjectId, 
       ref: 'autores', 
-      required: [true, "O(a) autor(a) é obrigatório"]},
+      required: [true, "O(a) autor(a) é obrigatório"]
+    },
     editora: {
       type: String, 
-      required: [true, "A editora é obrigatória"]},
-    numeroPaginas: {type: Number}
+      required: [true, "A editora é obrigatória"],
+      enum: {
+        values: ["Alura", "Edebê"],
+        message: "A editora {VALUE} não é um valor permitido."
+      }
+    },
+    numeroPaginas: {
+      type: Number,
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        message: "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"
+      }
+      /*min: [10, "O número de páginas deve estar entre 10 e 5000. O número {VALUE} não é um valor permitido"],
+      max: [5000, "O número de páginas deve estar entre 10 e 5000. O número {VALUE} não é um valor permitido"]*/
+    }
   }
 );
 
